37cb7fda97a670b643f1650874ab9bab847fdcc0
godlingswrath <godlingswrath@gmail.com>
2018-11-27 09:18:03 -0500

    file error handling

--- GMProject/main.py
+++ GMProject/main.py
@@ -16,8 +16,14 @@ with open('race_list.json', 'r') as data_file:
   race_list = json.load(data_file)
 with open('class_list.json', 'r') as data_file:
   class_list = json.load(data_file)
-with open('character_list.json', 'r') as data_file:
-  character_list = json.load(data_file)
+try:
+  with open('character_list.json', 'r') as data_file:
+    character_list = json.load(data_file)
+except  FileNotFoundError:
+  print('filenotfound')
+  character_list = ['nouserfound']
+  with io.open('character_list.json', 'w', encoding='utf8') as f:
+    f.write(json.dumps(character_list, ensure_ascii=False))
 #character creation
 def save_dict(fileName, dictName):
   with io.open(fileName, 'w', encoding='utf8') as outfile:
@@ -31,98 +37,101 @@ def save_dict(fileName, dictName):
 print('Hello what is your name?')
 myName = input()
 print('Good day, ' + myName + ' , welcome to the hero creator!')
-if myName not in character_list:
-  print('Lets Pick a Race!')
-#list race for choice
-  for list in race_list:
-      for x in list:
-          listed_races = race_list[x]
-          for z,y in listed_races.items():
+if 0 == 0:
+  if myName not in character_list:
+    print('Lets Pick a Race!')
+    print(character_list)
+    #list race for choice
+    for list in race_list:
+        for x in list:
+            listed_races = race_list[x]
+            for z,y in listed_races.items():
+              if z == "Name":
+                print(x + ":" + y)
+    pChoice = input()
+    myRace = race_list[str(pChoice)]
+    print('You Chose the ' + myRace["Name"] + ' Race!')
+    print('Lets Pick A Class!')
+    for dict in class_list.keys():
+        for x in dict:
+          listed_class = class_list[x]
+          for z,y in listed_class.items():
             if z == "Name":
-              print(x + ":" + y)
-  pChoice = input()
-  myRace = race_list[str(pChoice)]
-  print('You Chose the ' + myRace["Name"] + ' Race!')
-  print('Lets Pick A Class!')
-  for dict in class_list.keys():
-      for x in dict:
-        listed_class = class_list[x]
-        for z,y in listed_class.items():
-          if z == "Name":
-              print(x + ":" + y)
-  pChoice = input()
-  myClass = class_list[str(pChoice)]
-  print('You Chose the ' + myClass["Name"] + ' Class!')
-  print(myName + ' you have decided on a ' + myRace["Name"] + ":" + myClass["Name"])
-  print("OK, Let us find our ability scores!")
+                print(x + ":" + y)
+    pChoice = input()
+    myClass = class_list[str(pChoice)]
+    print('You Chose the ' + myClass["Name"] + ' Class!')
+    print(myName + ' you have decided on a ' + myRace["Name"] + ":" + myClass["Name"])
+    print("OK, Let us find our ability scores!")
 
-  dieRoll1 = random.randint(1,6)
-  dieRoll2 = random.randint(1,6)
-  dieRoll3 = random.randint(1,6)
-  dieRoll4 = random.randint(1,6)
-  rollList = [dieRoll1, dieRoll2, dieRoll3, dieRoll4]
-  print(rollList)
-  rollList.remove(min(rollList))
-  Strength = sum(rollList)
+    dieRoll1 = random.randint(1,6)
+    dieRoll2 = random.randint(1,6)
+    dieRoll3 = random.randint(1,6)
+    dieRoll4 = random.randint(1,6)
+    rollList = [dieRoll1, dieRoll2, dieRoll3, dieRoll4]
+    print(rollList)
+    rollList.remove(min(rollList))
+    Strength = sum(rollList)
 
-  dieRoll1 = random.randint(1,6)
-  dieRoll2 = random.randint(1,6)
-  dieRoll3 = random.randint(1,6)
-  dieRoll4 = random.randint(1,6)
-  rollList = [dieRoll1, dieRoll2, dieRoll3, dieRoll4]
-  print(rollList)
-  rollList.remove(min(rollList))
-  Dexterity = sum(rollList)
+    dieRoll1 = random.randint(1,6)
+    dieRoll2 = random.randint(1,6)
+    dieRoll3 = random.randint(1,6)
+    dieRoll4 = random.randint(1,6)
+    rollList = [dieRoll1, dieRoll2, dieRoll3, dieRoll4]
+    print(rollList)
+    rollList.remove(min(rollList))
+    Dexterity = sum(rollList)
 
-  dieRoll1 = random.randint(1,6)
-  dieRoll2 = random.randint(1,6)
-  dieRoll3 = random.randint(1,6)
-  dieRoll4 = random.randint(1,6)
-  rollList = [dieRoll1, dieRoll2, dieRoll3, dieRoll4]
-  print(rollList)
-  rollList.remove(min(rollList))
-  Constitution = sum(rollList)
+    dieRoll1 = random.randint(1,6)
+    dieRoll2 = random.randint(1,6)
+    dieRoll3 = random.randint(1,6)
+    dieRoll4 = random.randint(1,6)
+    rollList = [dieRoll1, dieRoll2, dieRoll3, dieRoll4]
+    print(rollList)
+    rollList.remove(min(rollList))
+    Constitution = sum(rollList)
 
-  dieRoll1 = random.randint(1,6)
-  dieRoll2 = random.randint(1,6)
-  dieRoll3 = random.randint(1,6)
-  dieRoll4 = random.randint(1,6)
-  rollList = [dieRoll1, dieRoll2, dieRoll3, dieRoll4]
-  print(rollList)
-  rollList.remove(min(rollList))
-  Intelligence = sum(rollList)
+    dieRoll1 = random.randint(1,6)
+    dieRoll2 = random.randint(1,6)
+    dieRoll3 = random.randint(1,6)
+    dieRoll4 = random.randint(1,6)
+    rollList = [dieRoll1, dieRoll2, dieRoll3, dieRoll4]
+    print(rollList)
+    rollList.remove(min(rollList))
+    Intelligence = sum(rollList)
 
-  dieRoll1 = random.randint(1,6)
-  dieRoll2 = random.randint(1,6)
-  dieRoll3 = random.randint(1,6)
-  dieRoll4 = random.randint(1,6)
-  rollList = [dieRoll1, dieRoll2, dieRoll3, dieRoll4]
-  print(rollList)
-  rollList.remove(min(rollList))
-  Wisdom = sum(rollList)
+    dieRoll1 = random.randint(1,6)
+    dieRoll2 = random.randint(1,6)
+    dieRoll3 = random.randint(1,6)
+    dieRoll4 = random.randint(1,6)
+    rollList = [dieRoll1, dieRoll2, dieRoll3, dieRoll4]
+    print(rollList)
+    rollList.remove(min(rollList))
+    Wisdom = sum(rollList)
 
-  dieRoll1 = random.randint(1,6)
-  dieRoll2 = random.randint(1,6)
-  dieRoll3 = random.randint(1,6)
-  dieRoll4 = random.randint(1,6)
-  rollList = [dieRoll1, dieRoll2, dieRoll3, dieRoll4]
-  print(rollList)
-  rollList.remove(min(rollList))
-  Charisma = sum(rollList)
-  abilityScores = {"Strength" : Strength, "Dexterity" : Dexterity, "Constitution" : Constitution, "Intelligence" : Intelligence, "Wisdom" : Wisdom, "Charisma" : Charisma}
-  for a in abilityScores.items():
-    print(str(a))
-  character_list.append(myName)
-  myDict = {"Name" : myName, "Race" : myRace, "Class" : myClass, "Ability Scores" : abilityScores}
-  save_dict(myName + '_info.json', myDict)
-  with io.open('character_list.json', 'w', encoding='utf8') as f:
-    f.write(json.dumps(character_list, ensure_ascii=False))
+    dieRoll1 = random.randint(1,6)
+    dieRoll2 = random.randint(1,6)
+    dieRoll3 = random.randint(1,6)
+    dieRoll4 = random.randint(1,6)
+    rollList = [dieRoll1, dieRoll2, dieRoll3, dieRoll4]
+    print(rollList)
+    rollList.remove(min(rollList))
+    Charisma = sum(rollList)
+    abilityScores = {"Strength" : Strength, "Dexterity" : Dexterity, "Constitution" : Constitution, "Intelligence" : Intelligence, "Wisdom" : Wisdom, "Charisma" : Charisma}
+    for a in abilityScores.items():
+      print(str(a))
+    character_list.append(myName)
+    if 'nouserfound' in character_list:
+      character_list.remove('nouserfound')
+    myDict = {"Name" : myName, "Race" : myRace, "Class" : myClass, "Ability Scores" : abilityScores}
+    save_dict(myName + '_info.json', myDict)
+    with io.open('character_list.json', 'w', encoding='utf8') as f:
+      f.write(json.dumps(character_list, ensure_ascii=False))
+  else:
+    print('You have a Character!')
+  print(character_list)
 else:
-  print('You have a Character!')
-
-
-
-print(character_list)
+  print("god is dead")
 #rollabilityscores
 #racelist
 #classlist
